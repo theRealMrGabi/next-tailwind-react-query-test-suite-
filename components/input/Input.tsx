a8f028f60ff60ce4b3c45a9e5d2756d34bc35d51
therealmrgabi <gabirated@gmail.com>
2021-11-17 10:52:25 +0100

    conditionally render input prefix

--- components/input/Input.tsx
+++ components/input/Input.tsx
@@ -11,44 +11,32 @@ interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
 export const Input = forwardRef<HTMLInputElement, InputProps>(
 	({ name, label, prefix, ...rest }, ref) => {
 		return (
-			<>
-				<div className="">
-					<label htmlFor={name}>{label}</label>
-					<div className="flex flex-row items-center">
+			<div className="w-full">
+				<label htmlFor={name}>{label}</label>
+				<div className="flex flex-row items-center">
+					{prefix && (
 						<span className="text-sm border rounded-l p-4 bg-gray-300 whitespace-no-wrap">
 							{prefix}
 						</span>
+					)}
 
-						<input
-							{...rest}
-							name={name}
-							ref={ref}
-							type={rest.type}
-							className={`border border-black rounded focus:outline-none p-3 ${
-								rest.type === "search" && `input-with-search rounded-r-none`
-							} ${prefix && `prefix rounded-l-none`}`}
-						/>
+					<input
+						{...rest}
+						name={name}
+						ref={ref}
+						type={rest.type}
+						className={`border border-black rounded focus:outline-none p-3 ${
+							rest.type === "search" && `border-r-0 rounded-r-none`
+						} ${prefix && `border-l-0 rounded-l-none`}`}
+					/>
 
-						{rest.type === "search" && (
-							<span className="border border-black rounded p-4 search rounded-l-none">
-								<SearchIcon />
-							</span>
-						)}
-					</div>
+					{rest.type === "search" && (
+						<span className="border border-black rounded p-4 border-l-0 rounded-l-none">
+							<SearchIcon />
+						</span>
+					)}
 				</div>
-
-				<style jsx>{`
-					.search {
-						border-left: none;
-					}
-					.prefix {
-						border-left: none;
-					}
-					.input-with-search {
-						border-right: none;
-					}
-				`}</style>
-			</>
+			</div>
 		);
 	}
 );
